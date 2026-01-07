7cbae30eca573c73c8b539e8732799d05d3d60cd
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-01-07 22:41:00 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 2 {
         println!("Usage : jlox [script]");
+        println!("...");
         exit(64);
     } else if args.len() == 2 {
         match run_file(&args[1]) {
